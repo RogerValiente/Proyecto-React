3c2b437db7fc5143006cc6de109e6af311fae992
RogerValiente <64560821+RogerValiente@users.noreply.github.com>
2021-02-23 21:58:52 -0500

    Actualizando

---
+++ App.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NavbarComponent from "./components/NavBar/index";
+import ItemListContainer from "./containers/ItemListContainer/index";
+import ItemDetailContainer from "./containers/ItemDetailContainer/index";
+import Cart from "./components/Cart/Index";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <NavbarComponent />
+        <Switch>
+          <Route path="/producto/:categoria/:id">
+            <ItemDetailContainer />
+          </Route>
+          <Route path="/producto/:id">
+            <ItemDetailContainer />
+          </Route>
+          <Route exact path="/carrito">
+            <Cart />
+          </Route>
+          <Route exact path="/catalog/:categoria">
+            <ItemListContainer />
+          </Route>
+          <Route exact path="/catalog">
+            <ItemListContainer />
+          </Route>
+          <Route exact path="/">
+            <ItemListContainer />
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
